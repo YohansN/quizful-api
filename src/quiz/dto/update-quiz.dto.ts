69dede1d28353fc86ddbaef97523e42627309ab3
YohansN <yohans.dev@gmail.com>
2025-03-23 15:28:43 -0300

    feat: initial mock controller

--- src/quiz/dto/update-quiz.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateQuizDto } from './create-quiz.dto';
-
-export class UpdateQuizDto extends PartialType(CreateQuizDto) {
-  id: number;
-}
--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { QuizGateway } from './quiz.gateway';
 import { LlmService } from 'src/llm/llm.service';
+import { QuizController } from './quiz.controller';
 
 @Module({
   providers: [QuizGateway, QuizService, LlmService],
+  controllers: [QuizController],
 })
 export class QuizModule {}
