80ce12c75b3d655eab46d5ae89f9209b9fbddbc2
YohansN <yohans.dev@gmail.com>
2025-04-18 16:54:07 -0300

    feat: added initial user CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -8,13 +8,14 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    const newUser = this.userService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    const newUser = await this.userService.create(createUserDto);
     return {
       message: "Usuário criado com sucesso!",
       user: {
         id: newUser.id,
         email: newUser.email,
+        username: newUser.username,
       },
     };
   }
@@ -24,18 +25,28 @@ export class UserController {
     return this.userService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @Get('email')
+  findOneByEmail(@Query('email') id: string) {
+    return this.userService.findOneByEmail(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+  @Get(':id')
+  findOneById(@Param('id') id: string) {
+    return this.userService.findOneById(id);
   }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.userService.update(+id, updateUserDto);
+  // }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+  const responde = this.userService.remove(id);
+  return {
+    message: "Usuário deletado com sucesso!",
+    id: id,
+    }
   }
+
 }
---
+++ src/user/user.repository.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service'; // ajuste o caminho
+
+@Injectable()
+export class UserRepository{
+    constructor(private readonly PrismaService: PrismaService) {}
+    
+    async createUser(id: string, username: string, email: string, password: string) {
+        const user = await this.PrismaService.user.create({
+        data: {
+            id,
+            username,
+            email,
+            password,
+        },
+        });
+        return;
+    }
+
+    async findAllUsers() {
+        return await this.PrismaService.user.findMany();
+    }
+    
+    async findUserById(id: string) {
+        const user = await this.PrismaService.user.findUnique({
+        where: { id },
+        });
+        return user;
+    }
+
+    async findUserByEmail(email: string) {
+        const user = await this.PrismaService.user.findFirst({
+        where: { email },
+        });
+        return user;
+    }
+
+    async deleteUser(id: string) {
+        await this.PrismaService.user.delete({
+        where: { id },
+        });
+    }
+    
+}
