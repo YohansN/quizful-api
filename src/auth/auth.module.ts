eb73bd3e77d11199bace5877e858379ca94c9672
YohansN <yohans.dev@gmail.com>
2025-04-19 12:02:53 -0300

    feat: added initial jwt token auth

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+
+@Module({
+    controllers: [AuthController],
+    providers: [AuthService],
+    imports: [UserModule, 
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+    })],
+})
+export class AuthModule {}
