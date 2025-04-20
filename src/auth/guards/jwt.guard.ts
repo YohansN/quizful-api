b30f9b00150e43f5c700b7e21b2931fa893b1304
YohansN <yohans.dev@gmail.com>
2025-04-20 10:59:27 -0300

    feat: added jwt strategy to validate token

--- src/auth/guards/jwt.guard.ts
+++ src/auth/guards/jwt.guard.ts
@@ -3,7 +3,7 @@ import { AuthGuard } from "@nestjs/passport";
 import { Observable } from "rxjs";
 
 @Injectable()
-export class LocalGuard extends AuthGuard('local') {
+export class JwtAuthGuard extends AuthGuard('jwt') {
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         return super.canActivate(context);
     }
