2b0b8771fea68a3ffebd55339a9f57b63e159bb7
YohansN <yohans.dev@gmail.com>
2025-04-20 14:09:01 -0300

    feat: added cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000', // URL do frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 2999);
 }
